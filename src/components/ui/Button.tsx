100e27fb3860c039b1b3ffa6695b43a5fa71a9b6
solomonrazak <solomonrazak99@gmail.com>
2025-02-25 19:30:24 -0800

    deployment issues fixed again

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,10 +5,10 @@ interface IButtonProps {
     name: string;
     className?: string;
     icon: React.ReactNode;
-    onClick?: () => void;
+    
 }
 
-const Button: React.FC<IButtonProps> = ({name, className, icon, onClick}) => {
+const Button: React.FC<IButtonProps> = ({name, className, icon, }) => {
   return (
     <button className={`bg-[#800020] text-white px-5 py-1 h-9 flex gap-2 items-center ${className}`}>{name}{icon}</button>
   )
